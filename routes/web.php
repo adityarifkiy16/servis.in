185fb279d0c9a78808f4d515f7b8e146cfd672c1
aditya <adityarifkistemba@gmail.com>
2025-10-06 15:19:39 +0700

    fix : change route default

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/admin', function () {
-    return view('admin');
-});
-
 Route::middleware(['guest'])->group(function () {
-    Route::get('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
+    Route::get('/', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
     Route::post('/login', [App\Http\Controllers\AuthController::class, 'authenticate'])->name('login.post');
 });
-
-
-
 
 Route::middleware(['auth'])->group(function () {
     Route::prefix('user')->group(function () {
