0f0a690bdf7643ed430283b85c443604f5de0bbe
aditya <adityarifkistemba@gmail.com>
2025-11-24 14:35:37 +0700

    feat: add icon on title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ env('APP_NAME') }} - @yield('title', 'Dashboard')</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo-dark.svg') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logo-dark.svg') }}">
 
     <!-- Include CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
